6a13f9a0eac70224835f6c8152c2af331751f4ff
Masato <4653960+M-Yamashita01@users.noreply.github.com>
2021-05-25 08:20:14 +0900

    Add test to execute multiple thread for deadlock.

--- RailsSample4/app/models/user.rb
+++ RailsSample4/app/models/user.rb
@@ -1,2 +1,53 @@
 class User < ActiveRecord::Base
+
+  @@thread_1_updated = false
+  @@thread_2_updated = false
+
+  def run_thread
+    threads = []
+    threads.push(t1_thread)
+    threads.push(t2_thread)
+
+    threads.each {|t| t.value}
+  end
+
+  def t1_thread
+    Thread.new do
+      ActiveRecord::Base.transaction do
+        puts 'Thread1 started update record'
+        # User.where(id: 0).update_all(name: 'baz')
+        puts 'Thread1 finished update record'
+
+        puts 'Thread1 started insert record'
+        for num in 1..2 do
+          new_user = User.new
+          new_user.name = 'hoge'
+          new_user.email = 'hoge@example.com'
+          new_user.save!
+          puts 'Thread1 inserted record'
+        end
+      end
+      puts 'Thread1 commit'
+    end
+  end
+
+  def t2_thread
+    Thread.new do
+      ActiveRecord::Base.transaction do
+        puts 'Thread2 started update record'
+        User.where(id: 1).update_all(name: 'test')
+        puts 'Thread2 finished update record'
+
+        puts 'Thread2 started insert record'
+        for num in 1..2 do
+          new_user = User.new
+          new_user.name = 'sample'
+          new_user.email = 'sample@example.com'
+          new_user.save!
+          puts 'Thread2 inserted record'
+        end
+      end
+      puts 'Thread2 commit'
+    end
+  end
 end
--- RailsSample4/spec/models/user_spec.rb
+++ RailsSample4/spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'テスト' do
+    before do
+      # create_list(:user, 10)
+      create(:user)
+
+      # User.create_list(name: 'foo', email: 'foo@example.com', )
+      # User.create(name: 'bar', email: 'bar@example.com')
+    end
+    it '正常系' do
+      sleep 10
+      # described_class.new.run_thread
+    end
+  end
 end
