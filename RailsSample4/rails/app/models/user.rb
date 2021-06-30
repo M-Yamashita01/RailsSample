56c85f3450324cd85245ee7ad741f0a9d4c83c1c
Masato <4653960+M-Yamashita01@users.noreply.github.com>
2021-07-01 08:31:57 +0900

    Add rubocop gem to check code by Style/NegatedIfElseCondition.

--- RailsSample4/rails/app/models/user.rb
+++ RailsSample4/rails/app/models/user.rb
@@ -5,50 +5,30 @@ class User < ActiveRecord::Base
 
     puts "User.count before thread: #{User.count}"
 
-    threads.push(thread_1)
-    threads.push(thread_2)
+    threads.push(thread1)
+    threads.push(thread2)
     threads.each { |t| t.value }
 
     puts "User.count after thread: #{User.count}"
+
+    test_flag = false
+    if !test_flag
+      test_flag = true
+      puts "test_flag is #{test_flag}"
+    else
+      puts 'test_flag is still false'
+    end
   end
 
-  def thread_1
+  def thread1
     Thread.new do
-      # if User.count.positive?
-      #   puts "User.count in thread_1: #{User.count}"
-      #   puts "User.last.inspect in thread_1: #{User.last.inspect}"
-      # else
-      #   puts 'No user in thread_1'
-      # end
       show_user_records('thread_1')
     end
-    # Thread.new do
-      # ActiveRecord::Base.transaction do
-      #   puts 'Thread1 started update record'
-      #   puts 'Thread1 finished update record'
-
-      #   puts 'Thread1 started insert record'
-      #   for num in 1..2 do
-      #     new_user = User.new
-      #     new_user.name = 'hoge'
-      #     new_user.email = 'hoge@example.com'
-      #     new_user.save!
-      #     puts 'Thread1 inserted record'
-      #   end
-      # end
-      # puts 'Thread1 commit'
-    # end
   end
 
-  def thread_2
+  def thread2
     Thread.new do
       show_user_records('thread_2')
-      # if User.count.positive?
-      #   puts "User.count in thread_2: #{User.count}"
-      #   puts "User.last.inspect in thread_2: #{User.last.inspect}"
-      # else
-      #   puts 'No user in thread_2'
-      # end
     end
   end
 
