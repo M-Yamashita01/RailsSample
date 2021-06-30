9959d7e841db052ff476382906763d2b1e940765
Masato <4653960+M-Yamashita01@users.noreply.github.com>
2021-07-01 06:13:38 +0900

    Use transaction mode for databasecleaner.

--- RailsSample4/rails/spec/rails_helper.rb
+++ RailsSample4/rails/spec/rails_helper.rb
@@ -37,8 +37,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  # config.use_transactional_fixtures = true
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
@@ -64,8 +63,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
-    # DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
